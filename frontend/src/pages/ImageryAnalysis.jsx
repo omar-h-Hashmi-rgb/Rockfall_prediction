781f8779fe8d9f2e29c13bfa4f992111a6003c7d
omar-H-hashmi-rgb <195132073+omar-h-Hashmi-rgb@users.noreply.github.com>
2025-09-09 15:20:53 +0530

    made some errors in frontend

---
+++ frontend/src/pages/ImageryAnalysis.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ImageryAnalysis() {
+  return (
+    <div style={{ padding: '2rem', color: 'white' }}>
+      <h1>📸 Imagery Analysis</h1>
+      <p>Drone imagery analysis and processing</p>
+    </div>
+  )
+}
+
+export default ImageryAnalysis
